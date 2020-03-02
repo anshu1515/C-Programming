a4fdf5234e43b52a0cf6f4e7c9cd7c975ee1027a
anshu1515 <anshubansal595@gmail.com>
2020-03-02 22:08:39 +0530

    Area of Equilateral Triangle

---
+++ Getting Started/area_equilateral_triangle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    int b;
+    float a;
+    scanf("%d",&b);
+    a=(sqrt(3)/4)*b*b;
+    printf("area of equilateral triangle:%.2f",a);
+    return 0;
+}
