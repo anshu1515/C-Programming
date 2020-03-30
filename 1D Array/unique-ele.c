d41782f48320f11f3d7240bbf4f89f009dfe5b64
anshu1515 <anshubansal595@gmail.com>
2020-03-30 16:16:51 +0530

    Unique Elements in Array

---
+++ 1D Array/unique-ele.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main()
+{
+int i,j,size,count;
+scanf("%d",&size);
+int fr[100];
+int ar[100];
+for(i=0;i<size;i++)
+{
+    scanf("%d",&ar[i]);
+    fr[i]=-1;
+}
+for(i=0;i<size;i++)
+{
+count=1;
+for(j=i+1;j<size;j++)
+{
+    if(ar[i]==ar[j])
+    {
+        count++;
+        fr[j]=0;
+    }
+}
+if(fr[i]!=0)
+{
+    fr[i]=count;
+}
+}
+for(i=0;i<size;i++)
+{
+    if(fr[i]==1)
+    {
+    printf("%d ",ar[i]);
+    }
+}
+    return 0;
+}
