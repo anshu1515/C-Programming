355e3d3b15cadbe2403574c1fc65dcb42c61ef06
anshu1515 <anshubansal595@gmail.com>
2020-03-30 16:33:15 +0530

    Add Two Matrices

---
+++ 2D Array/add-two-matrices.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main()
+{
+int i,j,n;
+scanf("%d",&n);
+int arr1[100][100];
+int arr2[100][100];
+int arr3[100][100];
+for(i=0;i<n;i++)
+{
+    for(j=0;j<n;j++)
+    {
+    scanf("%d",&arr1[i][j]);
+    }
+}
+for(i=0;i<n;i++)
+{
+    for(j=0;j<n;j++)
+    {
+   scanf("%d",&arr2[i][j]);
+    }
+}
+for(i=0;i<n;i++)
+{
+    for(j=0;j<n;j++)
+    {
+        arr3[i][j]=arr1[i][j]+arr2[i][j];
+    }
+}
+for(i=0;i<n;i++)
+{
+    for(j=0;j<n;j++)
+    {
+        printf("%d ",arr3[i][j]);
+    }
+    printf("\n");
+}
+return 0;
+}
